53f595b0e73fc535af6ffe33200314d946bf61de
Dumail <2439843430@qq.com>
2019-04-28 19:50:01 +0800

    init-packages.el

--- init.el
+++ init.el
@@ -1,51 +1,18 @@
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(when (>= emacs-major-version 24)
-     (require 'package)
-     (package-initialize)
-     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-		      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+(package-initialize)
 
-;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+(add-to-list 'load-path "~/.emacs.d/init")
+(require 'init-packages)
 
- ;; cl - Common Lisp Extension
- (require 'cl)
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 
- ;; Add Packages
- (defvar my/packages '(
-		;; --- Auto-completion ---
-		company
-		;; --- Better Editor ---
-		hungry-delete
-		swiper
-		counsel
-		smartparens
-		;; --- Major Mode ---
-		;; --- Minor Mode ---
-		;; --- Themes ---
-		monokai-theme
-		;; solarized-theme
-		) "Default packages")
-
- (setq package-selected-packages my/packages)
-
- (defun my/packages-installed-p ()
-     (loop for pkg in my/packages
-	   when (not (package-installed-p pkg)) do (return nil)
-	   finally (return t)))
-
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg my/packages)
-       (when (not (package-installed-p pkg))
-	 (package-install pkg))))
-
- ;; Find Executable Path on OS X
- (when (memq window-system '(mac ns))
-   (exec-path-from-shell-initialize))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -54,7 +21,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages (quote (company))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
+ ;; custom-set-faces was aX1dded by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
@@ -64,8 +31,7 @@
 (setq-default inhibit-splash-screen 1)
 (set-default-font "-ADBO-Source Code Variable-semibold-normal-normal-*-20-*-*-*-m-0-iso10646-1")
 
-;;打开n补全
-(global-company-mode 1)
+
 ;;关闭工具栏
 (tool-bar-mode -1)
 ;;关闭滑动条
@@ -104,16 +70,13 @@
 ;;高亮当前行
 (global-hl-line-mode t)
 
-;;设置主题
-(load-theme 'monokai 1)
 
-;;开启饥饿删除
-(require 'hungry-delete)
-(global-hungry-delete-mode)
 
-;;设置swiper
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
+
+;;自动更新配置文件的修改
+(global-auto-revert-mode t)
+
+
 (setq enable-recursive-minibuffers t)
 ;; enable this if you want `swiper' to use it
 ;; (setq search-default-mode #'char-fold-to-regexp)
@@ -125,8 +88,10 @@
 (global-set-key (kbd "C-h f") 'counsel-describe-function)
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
 
-;;设置括号补全
-(require 'smartparens-config)
-;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
-
+;;设置快捷输入
+(abbrev-mode)
+(define-abbrev-table 'global-abbrev-table'(
+					   ;;singnature
+					   ("pcf" "panchaofan")
+					   ;;emacs regex
+					   ))
